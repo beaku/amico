5c0484aba5a62d3603569849457fa82d7fafc229
Aayush <iit2017017@iiita.ac.in>
2018-10-20 15:05:26 +0530

    Components added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { VerificationComponent } from './components/verification/verification.component';
+import { DriverComponent } from './components/ride/driver/driver.component';
+import { PassengerComponent } from './components/ride/passenger/passenger.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LiveComponent } from './components/ride/live/live.component';
+import { EndComponent } from './components/ride/end/end.component';
+import { SplashComponent } from './components/splash/splash.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    VerificationComponent,
+    DriverComponent,
+    PassengerComponent,
+    ProfileComponent,
+    LiveComponent,
+    EndComponent,
+    SplashComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
